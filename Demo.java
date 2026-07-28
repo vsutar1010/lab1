7acb31e84b246522b6116fe07ec3745bf287a4f6
Krushnali Patil <2303058@ritindia.edu>
2026-07-28 16:21:06 +0530

    Changes made by krushnali

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 public class Demo{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("How are you guys?");
+        System.out.println("Im Krushnali");
     }
 }
