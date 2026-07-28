4346c08a7d9bb8426001598f1b65dbe8587cd49e
ashwin <baitadeashwin@gmail.com>
2026-07-28 16:44:55 +0530

    Changes made by Ashwin

---
+++ test1.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+        System.out.println("This is a test Java program.");
+    }
+}
